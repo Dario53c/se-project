a964d51c1c41dec22d7a5f3c5cd62c92225f4162
Dario Karadzin <dario.karadzin@stu.ibu.edu.ba>
2025-06-15 03:01:07 +0200

    swagger changes

--- php/doc.php
+++ php/doc.php
@@ -1,10 +1,18 @@
 <?php
-require("../vendor/autoload.php");
+require __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__ . '/swagger-base.php';
+/**
+ * @OA\Info(
+ *     title="SE Project API",
+ *     version="1.0.0",
+ *     description="Fitness Tracker API Documentation"
+ * )
+ * @OA\Server(
+ *     url="https://se-project-7kfh.onrender.com",
+ *     description="Production Server"
+ * )
+ */
 
-header('Content-Type: application/json');
-$openapi = \OpenApi\Generator::scan(['/var/www/html/php']);
-
+$openapi = \OpenApi\Generator::scan([__DIR__ . '/../php']); // Path to your controllers
 header('Content-Type: application/json');
 echo $openapi->toJson();
