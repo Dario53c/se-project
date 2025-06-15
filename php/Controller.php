640b92c5c04521ce879c616a7f48ba91251af6af
Dario Karadzin <dario.karadzin@stu.ibu.edu.ba>
2025-06-15 02:42:31 +0200

    changes reverted

--- php/Controller.php
+++ php/Controller.php
@@ -2,10 +2,7 @@
 namespace Sssd;
 
 use OpenApi\Annotations as OA;
-/**
- * @OA\Info(title="My API", version="1.0")
- * @OA\Server(url="https://se-project-7kfh.onrender.com", description="Production server")
- */
+
 class Controller {
     private $conn;
 
