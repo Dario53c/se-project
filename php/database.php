a5ee97e2175588e866decab80e5e04e373537e9e
Dario Karadzin <dario.karadzin@stu.ibu.edu.ba>
2025-06-15 03:29:46 +0200

    sdwadaw

--- php/database.php
+++ php/database.php
@@ -1,6 +1,19 @@
 <?php
 declare(strict_types=1);
 
+// CORS Headers (must be at the very top, before any output)
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json");
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/config.php';
 
