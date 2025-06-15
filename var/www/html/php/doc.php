a07a7a40e5b760a7330a65104290d642f592614e
Dario Karadzin <dario.karadzin@stu.ibu.edu.ba>
2025-06-15 02:09:15 +0200

    sdawdwawsadwadasdwadwad

--- var/www/html/php/doc.php
+++ var/www/html/php/doc.php
@@ -3,7 +3,7 @@ require("../vendor/autoload.php");
 
 // Scan the directory containing your Controller.php
 $openapi = \OpenApi\Generator::scan([
-    __DIR__ . '/../php'  // Directory containing Controller.php
+    'C:\xampp\htdocs\se-project\php'  // Directory containing Controller.php
 ]);
 
 header('Content-Type: application/json');
